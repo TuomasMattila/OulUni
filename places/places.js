5c76102ffc07cfe4c78ecf5a42b9784bba0052c2
TuomasMattila <tuomas.mattila@windowslive.com>
2021-05-16 17:18:31 +0300

    updates on places page

---
+++ places/places.js
@@ -0,0 +1,15 @@
+/*var clicked = false;
+
+let categoryButton = document.querySelector('#category-button');
+
+categoryButton.addEventListener('click', toggleButton());
+
+function toggleButton() {
+    if(!clicked) {
+        clicked = true;
+        text = document.getElementById('category-button').innerHTML += "✓";
+    } else {
+        clicked = false;
+        document.getElementById('category-button').innerHTML -= "✓";
+    }
+}*/
