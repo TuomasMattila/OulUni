c6317d39d4cc2c5397a96dbe413ae85d5fbd0f9b
TuomasMattila <tuomas.mattila@windowslive.com>
2021-05-17 10:38:14 +0300

    category buttons work visually, except hovering effect does not work after click

--- places/places.js
+++ places/places.js
@@ -1,15 +1,24 @@
-/*var clicked = false;
-
-let categoryButton = document.querySelector('#category-button');
-
-categoryButton.addEventListener('click', toggleButton());
-
-function toggleButton() {
-    if(!clicked) {
-        clicked = true;
-        text = document.getElementById('category-button').innerHTML += "✓";
+function toggleButton(elementId) {
+    var btn = document.getElementById(elementId);
+    if(btn.innerHTML.includes("✓")) {
+        btn.innerHTML = btn.innerHTML.replace(" ✓", "");
+        btn.style.backgroundColor = "darkgrey";
+        btn.style.color = "grey";
     } else {
-        clicked = false;
-        document.getElementById('category-button').innerHTML -= "✓";
+        btn.innerHTML += " ✓";
+        btn.style.color = "black";
+        switch(elementId) {
+            case "attractions-button":  btn.style.backgroundColor = "rgba(211, 192, 127, 1)";
+                                        break;
+            case "restaurants-button":  btn.style.backgroundColor = "rgba(211, 127, 127, 1)";
+                                        break;
+            case "partyspots-button":   btn.style.backgroundColor = "rgba(127, 211, 176, 1)";
+                                        break;
+            case "clubs-button":        btn.style.backgroundColor = "rgba(199, 127, 211, 1)";
+                                        break;
+            case "campuses-button":     btn.style.backgroundColor = "rgb(82, 94, 160)";
+                                        break;
+            default: break;
+        }
     }
-}*/
+}
