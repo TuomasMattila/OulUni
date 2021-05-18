f6b47c0f35b9ed8f607a5ca3ad5634e612411245
Juuso Luttinen <justus.luttinen@gmail.com>
2021-05-18 14:38:15 +0300

    reworked the adjective changing, new font styles

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
-var heyText = document.getElementById('heyBox').children[0];
-var heyTextList = ['Hey Stu&shy;dent!', 'Hei Op&shy;is&shy;ke&shy;li&shy;ja!', 'Hej Stu&shy;de&shy;ran&shy;de!']
-var selectedHeyListIndex = 0;
-heyText.innerHTML = `${heyTextList[selectedHeyListIndex]}`;
+// Class that has the adjectives text and color.
+//
+class AdjectiveText {
+    constructor(text, color, fontSize) {
+        this.text = text;
+        this.color = color;
+        if(fontSize == null) {
+            this.fontSize = 'var(--font-3xl)';
+        } else {
+            this.fontSize = fontSize;
+        }
+    }
 
-var adjectiveText = document.getElementById('adjective').children[0];
-var adjectiveList = ['Wacky', 'Fun', 'Drunken']
-var selectedAdjListIndex = 0;
-adjectiveText.innerHTML = `${adjectiveList[selectedHeyListIndex]}`;
+}
+
+// Creating an array which has the adjectives and their colors.
+//
+let listOfAdjectives = [];
+listOfAdjectives.push(new AdjectiveText('Wacky', 'var(--color-primary)'));
+listOfAdjectives.push(new AdjectiveText('Fun', '#7FD387'));
+listOfAdjectives.push(new AdjectiveText('Drunk', '#D37F98'));
+listOfAdjectives.push(new AdjectiveText('Drunker', '#AE7FD3'));
+listOfAdjectives.push(new AdjectiveText('Extra-<br>ordinary', '#7F91D3', 'var(--font-2xl)'));
+
+let listIndex = 4;
+let adjectiveText = document.querySelector('#adjective > h2');
+let adjective = document.querySelector('#adjective');
+adjectiveText.innerHTML = `${listOfAdjectives[listIndex].text}`;
+adjectiveText.style.color = `${listOfAdjectives[listIndex].color}`;
+adjectiveText.style.fontSize = `${listOfAdjectives[listIndex].fontSize}`;
+
 
 
 // Interates throught a list of adjectives and changes the adjective
 // in home screen every 3 seconds.
 //
-function changeText() {
-    let interval = 3000;
-    let listLength = heyTextList.length;
-    let adjListLength = adjectiveList.length;
+function changeAdjective() {
+    let listLength = listOfAdjectives.length;
 
-    setInterval(() => {
-        selectedHeyListIndex += 1;
-        selectedAdjListIndex += 1;
-        /* if (selectedHeyListIndex >= 0 && selectedHeyListIndex < listLength) {
-            heyText.innerHTML = `${heyTextList[selectedHeyListIndex]}`;
-        }
-        if (selectedHeyListIndex >= listLength) {
-            selectedHeyListIndex = 0;
-            heyText.innerHTML = `${heyTextList[selectedHeyListIndex]}`;
-        } */
-        if (selectedAdjListIndex >= 0 && selectedAdjListIndex < adjListLength) {
-            adjectiveText.innerHTML = `${adjectiveList[selectedAdjListIndex]}`;
-        }
-        if (selectedAdjListIndex >= adjListLength) {
-            selectedAdjListIndex = 0;
-            adjectiveText.innerHTML = `${adjectiveList[selectedAdjListIndex]}`;
-        }
+    adjective.classList.add('hide');
 
-    }, interval);
+    setTimeout(() => {
+        adjective.classList.remove('hide');
+        listIndex += 1;
+        if (listIndex >= listLength) {
+            listIndex = 0;
+        }
+        adjectiveText.innerHTML = `${listOfAdjectives[listIndex].text}`;
+        adjectiveText.style.color = `${listOfAdjectives[listIndex].color}`;
+        adjectiveText.style.fontSize = `${listOfAdjectives[listIndex].fontSize}`;
+        //console.log(listIndex);
+    }, 500);
+
+
 }
+
+
 
 // Starts changing the adjectives
 //
-changeText();
+setInterval(changeAdjective, 3000);
 
 
 
