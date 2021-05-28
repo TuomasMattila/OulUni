174d97d8a9d4e2b077ae6625a7c648c3f5cbd12f
Juuso Luttinen <justus.luttinen@gmail.com>
2021-05-28 18:29:26 +0300

    map info popup now has real address and description

--- map/map.js
+++ map/map.js
@@ -38,42 +38,61 @@ const teekkaritaloLoc = { lat: 65.063879, lng: 25.484157 };
 const toripolliisiLoc = { lat: 65.013306, lng: 25.464720 };
 const yliopistoLoc = { lat: 65.059316, lng: 25.466266 };
 
-const apinapatsasCont = 'The monkey statue or by its official name: "The thirst for knowledge" is a cast bronze statue of an orangutan reading a book. ' +
-  'It was designed by a local comic artist Raimo Mersänheimo in 1987 and according to him it depicts an animal becoming a thinking being. ' +
+const apinapatsasAddress = 'Kalevantie 5, Kaijonharju';
+const hoyhtyanGrilliAddress = 'Latokartanontie 1, 90150 Oulu';
+const laakisAddress = 'Aapistie 5 A, 90220 Oulu';
+const kauppuri5Address = 'Kauppurienkatu 5, 90100 Oulu';
+const kontinkangasAMKAddress = 'Kiviharjuntie 4, 90220 Oulu';
+const paskaKaupunniAddress = 'Uusikatu 22, 90100 Oulu';
+const teekkaritaloAddress = 'Kalervontie 7, 90570 Oulu';
+const toripolliisiAddress = 'Rantakatu 6, 90100 Oulu';
+const yliopistoAddress = 'Pentti Kaiteran katu 1, 90570 Oulu';
+
+const apinapatsasDesc = 'The monkey statue or by its official name: "The thirst for knowledge" is a cast bronze statue of an orangutan reading a book. '
+const apinapatsasCont = 'It was designed by a local comic artist Raimo Mersänheimo in 1987 and according to him it depicts an animal becoming a thinking being. ' +
   'The statue is located next to Kaijonharju library, near the Linnanmaa campus and is popular gathering spot among students.';
-const hoyhtyanGrilliCont = 'In finnish night culture grills are a mainstay. The best fix for midnight hunger in Oulu is the grill at Höyhtyä. ' +
-  'It boasts a large collection of  greasy delights such as burgers and stick kebabs.';
-const kauppuri5Cont = 'A legendary burger joint located in the centrum. Located in its namesake Kauppurienkatu 5. ' +
-  'It\'s legend is made of it\'s special attitude to making food as well as providing the best burgers in the city. ' +
+
+const hoyhtyanGrilliDesc = 'In finnish night culture grills are a mainstay. The best fix for midnight hunger in Oulu is the grill at Höyhtyä.';
+const hoyhtyanGrilliCont = 'It boasts a large collection of  greasy delights such as burgers and stick kebabs.';
+
+const kauppuri5Desc = 'A legendary burger joint located in the centrum. Located in its namesake Kauppurienkatu 5.';
+const kauppuri5Cont = 'It\'s legend is made of it\'s special attitude to making food as well as providing the best burgers in the city. ' +
   'The small burger restaurant\'s interior is covered from floor to ceiling in greetings and messages of its many customers with ' +
   'special places reserved for those of celebrities such as rockstars. ' +
   'Their slogan goes " Kauppuri 5! Nursing and aquiring hangovers since 2010!" ' +
   'Their menu includes hits like: "Blue cheese PERKELE!", "Angry Chicken", Kauppuri 5 and "The Butcher" line of burgers, ' +
   'largest of which contains an entire kilo of pulled pork, roast beef, minced meat and bacon.';
+
+const kontinkangasAMKDesc = 'Desc'
 const kontinkangasAMKCont = 'University of Applied Sciences campus at Kontinkangas.';
-const laakisCont = 'The secondary campus in Oulu. Medical and biochemical from the university and some from applied sciences dwell here. ' +
+
+const laakisDesc = 'The secondary campus in Oulu. Medical and biochemical from the university and some from applied sciences dwell here.';
+const laakisCont = 
   'Specializes in helping people due to being semi attached to the city\'s hospital.';
-const paskaKaupunniCont = 'How often does a piece of graphiti end up on a city\'s list of sights? Oulu is one of such and the spray in question is on the' +
-  ' wall of uusikatu 22 in central Oulu. ' +
-  'It is a stylized text that simply states: "Shitty city." It is inspired by a rocksong of the same name by Kauko Röyhkä who wrote ' +
+  
+const paskakaupunniDesc = 'How often does a piece of graphiti end up on a city\'s list of sights? Oulu is one of such and the spray in question is on the' +
+' wall of uusikatu 22 in central Oulu. ';
+const paskaKaupunniCont = 'It is a stylized text that simply states: "Shitty city." It is inspired by a rocksong of the same name by Kauko Röyhkä who wrote ' +
   'the song to lovingly commemorate his youth in Oulu.' +
   'It appeared on the wall in the mid eighties and despite several dozens of attempts to remove it from then to now, it always seems ' +
   'to reappear sooner or later.' +
   'It is also the namesake and logo of a local culture organization since 2007. ' +
   'A protected and reapplied landmark nowadays. ' +
   'Kinda has a point when it is made into an attraction?';
-const teekkaritaloCont = 'The official clubspot of the engineering students or more commonly the teekkaris in Oulu. ' +
-  'This partyhouse is often rented by other guilds too making it the premier place for student parties in Oulu. ' +
+
+const teekkaritaloDesc = 'The official clubspot of the engineering students or more commonly the teekkaris in Oulu.'
+const teekkaritaloCont = 'This partyhouse is often rented by other guilds too making it the premier place for student parties in Oulu. ' +
   'Has a great sauna because the idea of building it was concieved beer in hand in a sauna. ' +
   'Built by the teekkaris and by providing work for unemployed people in 1992.';
-const toripolliisiCont = 'Toripolliisi or in English: "Bobby at the market place" is possibly the most popular and quintessential landmark in Oulu. ' +
-  'It can be concidered an icon of the city itself. It is a bronze sculpture of a caricaturized policeman displayed at the edge of the market square.' +
+ 
+const toripolliisiDesc = 'Toripolliisi or in English: "Bobby at the market place" is possibly the most popular and quintessential landmark in Oulu.';
+const toripolliisiCont = 'It can be concidered an icon of the city itself. It is a bronze sculpture of a caricaturized policeman displayed at the edge of the market square.' +
   'Made via charity in 1985, unveiled in 1987. ' +
   'Stands in honour of the 3 bobbies who stood watch in the square for 44 years in the mid 20th century. ' +
   'Nothing special, yet the city\'s symbol and a must see for anyone visiting or new here. ';
-const yliopistoCont = '"Lipasto"  (the Linnanmaa campus) ' +
-  'Lipasto(the dresser) is the University of Oulu\'s and the Oulu University of Applied Sciences shared primary campus at Linnanmaa. ' +
-  'As such it is the primary center of learning and student activities in Oulu. ' +
+
+const yliopistoDesc = 'Lipasto(the dresser) is the University of Oulu\'s and the Oulu University of Applied Sciences shared primary campus at Linnanmaa.';
+const yliopistoCont = 'As such it is the primary center of learning and student activities in Oulu. ' +
   'The large buildings sheer area works as a good exercise when two lectures are on opposite ends and there is just 15 minutes between them. ' +
   'Cheap and decent food, quiet places for studying, others less so for spending time';
 
@@ -89,12 +108,12 @@ function initMap() {
   });
 
   const markers = [
-    ['Teekkaritalo', '../img/teekTal.jpg', teekkaritaloCont, '../img/mapIcons/teekkaritalo.svg', teekkaritaloLoc, map, 'partyspots'],
-    ['Toripolliisi', '../img/torpo.jpg', toripolliisiCont, '../img/mapIcons/toripolliisi.svg', toripolliisiLoc, map, 'attractions'],
-    ['Paska Kaupunni', '../img/paskaKaup.jpg', paskaKaupunniCont, '../img/mapIcons/paskakaupunni.svg', paskaKaupunniLoc, map, 'attractions'],
-    ['Apinapatsas', '../img/tiedonjano.jpg', apinapatsasCont, '../img/mapIcons/apinapatsas.svg', apinapatsasLoc, map, 'attractions'],
-    ['Höyhtyän grilli', '../img/hoyhtyanGrilli.jpg', hoyhtyanGrilliCont, '../img/mapIcons/hoyhtyangrilli.svg', hoyhtyanGrilliLoc, map, 'restaurants'],
-    ['Kauppuri 5', null, kauppuri5Cont, '../img/mapIcons/kauppuri5.svg', kauppuri5Loc, map, 'restaurants']
+    ['Teekkaritalo', '../img/teekTal.jpg', teekkaritaloCont, '../img/mapIcons/teekkaritalo.svg', teekkaritaloLoc, map, 'partyspots', teekkaritaloDesc, teekkaritaloAddress],
+    ['Toripolliisi', '../img/torpo.jpg', toripolliisiCont, '../img/mapIcons/toripolliisi.svg', toripolliisiLoc, map, 'attractions', toripolliisiDesc, toripolliisiAddress],
+    ['Paska Kaupunni', '../img/paskaKaup.jpg', paskaKaupunniCont, '../img/mapIcons/paskakaupunni.svg', paskaKaupunniLoc, map, 'attractions', paskakaupunniDesc, paskaKaupunniAddress],
+    ['Apinapatsas', '../img/tiedonjano.jpg', apinapatsasCont, '../img/mapIcons/apinapatsas.svg', apinapatsasLoc, map, 'attractions', apinapatsasDesc, apinapatsasAddress],
+    ['Höyhtyän grilli', '../img/hoyhtyanGrilli.jpg', hoyhtyanGrilliCont, '../img/mapIcons/hoyhtyangrilli.svg', hoyhtyanGrilliLoc, map, 'restaurants', hoyhtyanGrilliDesc, hoyhtyanGrilliAddress],
+    ['Kauppuri 5', null, kauppuri5Cont, '../img/mapIcons/kauppuri5.svg', kauppuri5Loc, map, 'restaurants', kauppuri5Desc, kauppuri5Address]
   ];
 
   for (let index = 0; index < markers.length; index++) {
@@ -161,6 +180,8 @@ function addMarkerWithWindow(markerInfo) {
   let coordinate = markerInfo[4];
   let map = markerInfo[5];
   let category = markerInfo[6];
+  let desc = markerInfo[7];
+  let address = markerInfo [8];
 
 
   let marker = new google.maps.Marker({
@@ -191,7 +212,7 @@ function addMarkerWithWindow(markerInfo) {
     <div id="topbar">
       <div id="titleContainer">
           <span id="placeName">${name}</span>
-          <span id="placeAddress">Osoite 10, 90530 Oulu</span>
+          <span id="placeAddress">${address}</span>
       </div>
       <button id="exitButton" onclick="closePopup()"><span class="material-icons">close</span></button>
     </div>
@@ -203,7 +224,7 @@ function addMarkerWithWindow(markerInfo) {
 
       <div id="bannerBox">
               <div id="shortDesc">
-                  <p>Lyhyt kuvaus paikasta. Lyhyt kuvaus paikasta. Lyhyt kuvaus paikasta. Lyhyt kuvaus paikasta. Lyhyt kuvaus paikasta.</p>
+                  <p>${desc}</p>
               </div>
               <button id="showOnMapButton">NAVIGATE TO</button>
       </div>
